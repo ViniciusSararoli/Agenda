b0efef203bb9f31f71ddae5b8332e7fafe05c6bb
Vinicius Sararoli de Souza <viniciussararoli@hotmail.com>
2021-07-10 03:24:18 -0300

    Adicionado index, feita conexão com o DB e feito a primeira roda de direcionamento

--- src/model/DAO.java
+++ src/model/DAO.java
@@ -1,5 +1,32 @@
 package model;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DAO {
+	private String url = "jdbc:mysql://localhost:3306/agenda";
+	private String user = "root";
+	private String password = "";
+	
+	private Connection conectar() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(url, user, password);
+			return con;
+		} catch (Exception e) {
+			System.out.println(e);
+			return null;
+		}
+	}
+	
+	/*public void testeConexao() {
+		try {
+			Connection con = conectar();
+			System.out.println(con);
+			con.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}*/
 }
